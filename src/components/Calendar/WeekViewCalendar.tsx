ea5ecb55abbcf0aea1aa888d8138b1e4b5b20154
Klaudia Popow <401462@student.agh.edu.pl>
2023-04-13 14:15:30 +0200

    Adding Initial React App setup

---
+++ src/components/Calendar/WeekViewCalendar.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export default function WeekViewCalendar() {
+
+    return (
+        <div className="week-view-calendar">
+            <div className="week-view-calendar__header"> CALENDAR HEADER </div>
+            <div className="week-view-calendar__table">
+                TABLE
+            </div>
+        </div>
+    );
+}
+
