82f2cf332efe17420ea324c87f6db30efbe795ff
Sophie Popow <sophiepopow@Sophies-MacBook-Air.local>
2023-10-07 22:06:34 +0200

    small tasks redesign and adding logout

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './NavigationMenu.css';
 import logo from '../../images/logo.png';
 import history from '../../images/history.png';
+import settings from '../../images/settings_icon.svg';
+import logout from '../../images/off2_icon.png';
 
 export function NavigationMenu() {
+    const navigate = useNavigate();
+
+    function logoutUser() {
+        localStorage.removeItem('jwt'); 
+        navigate("/login");
+    }
+
     return (
         <div className='menuContainer'>
-            <img className="logo" src={logo} alt="logo" />
-            <img className="logo" src={history} alt="logo" />
+            <div className='menuContainer_options'>
+                <img className="logo" src={logo} alt="ProductivityPal logo" />
+                <img className="logo" src={history} alt="Statistics and history" />
+            </div>
+            <img className="logo" src={logout} alt="Logout" onClick={() => {logoutUser()}} />
         </div>);
 }
