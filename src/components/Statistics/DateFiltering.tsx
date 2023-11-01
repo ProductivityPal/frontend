a6141b396e060a11ee15560b4dc7725e48f29db2
Sophie Popow <sophiepopow@Sophies-MacBook-Air.local>
2023-11-01 18:55:39 +0100

    Adding Date range picker

--- src/components/Statistics/DateFiltering.tsx
+++ src/components/Statistics/DateFiltering.tsx
@@ -1,9 +1,55 @@
 import React from "react";
-// import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { useState } from "react";
+import dayjs, { Dayjs } from 'dayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Button } from "@mui/material";
+import './DateFiltering.css';
+import { putData } from "../../utils/fetchUtils";
+
 
 export function DateFiltering() {
+    const [startDate, setStartDate] = useState<Dayjs>(dayjs().subtract(1, 'month'));
+    const [endDate, setEndDate] = useState<Dayjs>(dayjs());
+
+    const handleStartDateChange = (date: any) => {
+        setStartDate(date);
+    };
+
+    const handleEndDateChange = (date: any) => {
+        setEndDate(date);
+    };
+
+    const subtasksButton = {
+        backgroundColor: '#EE7F3B',
+        color: 'white',
+        "&:hover": {backgroundColor: "#F8DEB3"},
+        // width: '100%',
+    }
+
+    function updateDateRange() {
+        // a moze get?
+        // putData<{}, number>(`http://localhost:8080/dateRange`, { "startDate": startDate, "endDate": endDate })();
+    }
+
     return (
-        <div> Date Filtering Picker</div>
-        // <DateRangePicker localeText={{ start: 'Check-in', end: 'Check-out' }} />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div className="container">
+            <DatePicker
+                value={startDate}
+                onChange={handleStartDateChange}
+                format="DD/MM/YY"
+            />
+            <span> - </span>
+            <DatePicker
+                value={endDate}
+                onChange={handleEndDateChange}
+                minDate={startDate}
+                format="DD/MM/YY"
+            />
+            <Button sx={subtasksButton} disabled={endDate < startDate} onClick={() => updateDateRange()}>Update</Button>
+        </div>
+        </LocalizationProvider>
     )
 }
