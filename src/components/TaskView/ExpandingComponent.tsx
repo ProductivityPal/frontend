860dd3d761fdb0625332cb4410d9ea8f8ea45391
Sophie Popow <sophiepopow@Sophies-MacBook-Air.local>
2023-12-09 07:55:55 +0100

    fixing category popover

--- src/components/TaskView/ExpandingComponent.tsx
+++ src/components/TaskView/ExpandingComponent.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import { useState } from 'react'
-import expand from '../../images/expand_icon.svg'
+import React, { useState } from 'react';
 import './ExpandingComponent.css';
-import { Popover, dividerClasses } from '@mui/material';
-import { deleteData, putData } from '../../utils/fetchUtils';
-import { Button } from '@mui/material';
-import { Menu } from '@mui/material';
-import { postData } from "../../utils/fetchUtils";
+import { Popover, Button } from '@mui/material';
+import { deleteData, putData, postData } from '../../utils/fetchUtils';
 import { AddTaskModal } from './AddTaskModal';
 import { Task } from '../../types/Task';
 
@@ -23,7 +18,6 @@ type TaskViewProps = {
     duration?: number;
 }
 
-
 export function ExpandingComponent(props: TaskViewProps) {
     const [expanded, setExpanded] = useState(false)
     const [expandedCategory, setExpandedCategory] = useState(false)
@@ -33,15 +27,25 @@ export function ExpandingComponent(props: TaskViewProps) {
     const [subtasks, setSubtasks] = useState(props.subTasks ? props.subTasks : [])
     const [openAddTaskModal, setOpenAddTaskModal] = useState(false);
     const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+    const [anchorElCategory, setAnchorElCategory] = React.useState<HTMLButtonElement | null>(null);
+
+    const handlePopover = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorElCategory(anchorElCategory ? null : event.currentTarget);
+    }
+
+    const handleCloseCategory = () => {
+        setAnchorElCategory(null);
+    }
 
     const handleAddTaskClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
-    };
+    }
 
     const addNewTaskAction = (e: React.MouseEvent<HTMLButtonElement>) => {
         setOpenAddTaskModal(true);
         handleAddTaskClick(e);
     }
+
     const handleAddTaskClose = () => {
         setAnchorEl(null);
         setOpenAddTaskModal(false);
@@ -49,14 +53,11 @@ export function ExpandingComponent(props: TaskViewProps) {
 
     const handleClose = () => {
         setAnchorEl(null);
-    };
+    }
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
+        setAnchorEl(event.currentTarget)
+    }
 
     function completeTask(taskId: number) {
         console.log("complete Task!" + taskId)
@@ -64,38 +65,25 @@ export function ExpandingComponent(props: TaskViewProps) {
 
         props.onComplete()
     }
+
     function deleteTask(taskId: number) {
         console.log("delete Task!" + taskId)
         deleteData<{}, number>(`http://localhost:8080/task/${taskId}`, {})();
 
         props.onComplete()
     }
+
     const handleUpdateTask = (updatedTask: Task) => {
         props.onUpdateTask(updatedTask);
     }
 
-    function convertCategoryToColor(category: string) {
-        switch (category) {
-            case 'green':
-                return "#707247"
-            case 'accent':
-                return "#EE7F3B"
-            case 'grey':
-                return "#E5E5E5"
-            default:
-                return "#E7C3A1"
-        }
-
-    }
     function sendCategory(category: string) {
         // send category to backend
         putData<{}, number>(`http://localhost:8080/task/${props.task.id}`, { "category": category })();
         setCategory(category)
         setTaskColor(convertCategoryToColor(category))
-
-
-
     }
+
     function addSubtask(subtaskName: string) {
         const newSubtask = {
             name: subtaskName,
@@ -114,24 +102,6 @@ export function ExpandingComponent(props: TaskViewProps) {
 
     }
 
-    const buttonLowStyle = {
-        color: 'black',
-        maxWidth: '6px',
-        minWidth: '6px',
-        "&:hover": { backgroundColor: "#FFFFFF50" },
-    }
-
-    const accentStyle = {
-        color: '#EE7F3B',
-        // backgroundColor: "#EE7F3B20",
-        "&:hover": { backgroundColor: "#EE7F3B50" },
-    }
-    const dangerStyle = {
-        color: '#D83C1A',
-        // backgroundColor: "#D83C1A20",
-        "&:hover": { backgroundColor: "#D83C1A50" },
-    }
-
     return (
         <div className='expand-container' style={{
             height: props.duration ? props.duration : 'auto',
@@ -147,19 +117,33 @@ export function ExpandingComponent(props: TaskViewProps) {
                 {/* <Button sx={buttonLowStyle} onClick={() => deleteTask(props.task.id)}>x</Button> */}
                 {/* {props.isExpandable && <img className="expand-icon" src={expand} alt="expand tasks view" onClick={() => setExpanded(!expanded)} />} */}
                 {!props.task.completed && !props.isEditView && <Button className='basicButton' sx={buttonLowStyle} onClick={() => { completeTask(props.task.id) }}>✓</Button>}
-                <button className={`circleButton ${category}`} onClick={() => setExpandedCategory(!expandedCategory)}>
-                    {expandedCategory && <div className='category-menu'>
-                        <button className='circleButton' onClick={() => sendCategory('beige')} />
-                        <button className='circleButton green' onClick={() => sendCategory('green')} />
-                        <button className='circleButton accent' onClick={() => sendCategory('accent')} />
-                        <button className='circleButton grey' onClick={() => sendCategory('grey')} />
-                    </div>}
+                <button className={`circleButton ${category}`} onClick={handlePopover}>
+                    <Popover
+                        open={Boolean(anchorElCategory)}
+                        onClose={handleCloseCategory}
+                        anchorEl={anchorElCategory}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                        }}
+                        transformOrigin={{
+                            vertical: 'center',
+                            horizontal: 'left',
+                        }}
+                    >
+                        <div className='popover-menu'>
+                            <button className='circleButton' onClick={() => sendCategory('beige')} />
+                            <button className='circleButton green' onClick={() => sendCategory('green')} />
+                            <button className='circleButton accent' onClick={() => sendCategory('accent')} />
+                            <button className='circleButton grey' onClick={() => sendCategory('grey')} />
+                        </div>
+                    </Popover>
                 </button>
                 <div className='task-label'>{props.task.name}</div>
                 {/* {props.isEditView && <Button className='basicButton' sx={buttonLowStyle} onClick={addNewTaskAction}>✎</Button>} */}
-                <Button sx={buttonLowStyle} aria-describedby={id} onClick={handleClick}>...</Button>
+                <Button sx={buttonLowStyle} onClick={handleClick}>...</Button>
                 <Popover
-                    open={open}
+                    open={Boolean(anchorEl)}
                     onClose={handleClose}
                     anchorEl={anchorEl}
                     anchorOrigin={{
@@ -206,4 +190,36 @@ export function ExpandingComponent(props: TaskViewProps) {
 
         </div>
     );
+};
+
+function convertCategoryToColor(category: string) {
+    switch (category) {
+        case 'green':
+            return "#707247"
+        case 'accent':
+            return "#EE7F3B"
+        case 'grey':
+            return "#E5E5E5"
+        default:
+            return "#E7C3A1"
+    }
+}
+
+const buttonLowStyle = {
+    color: 'black',
+    maxWidth: '6px',
+    minWidth: '6px',
+    "&:hover": { backgroundColor: "#FFFFFF50" },
+}
+
+const accentStyle = {
+    color: '#EE7F3B',
+    // backgroundColor: "#EE7F3B20",
+    "&:hover": { backgroundColor: "#EE7F3B50" },
+}
+
+const dangerStyle = {
+    color: '#D83C1A',
+    // backgroundColor: "#D83C1A20",
+    "&:hover": { backgroundColor: "#D83C1A50" },
 }
